f0e049b5328de0a2dae82a3da6a8f86ea34b2bff
garyson0 <vengergo@gmail.com>
2024-07-12 18:42:40 +0300

    fix/backend changed the album controllers get method to retrieve albums of an artist

--- backend/controllers/albumController.js
+++ backend/controllers/albumController.js
@@ -1,11 +1,18 @@
-import Album from "../models/Album.js";
-
+import Artist from "../models/Artist.js";
 
 export const getAlbumsByArtistId = async (req, res) => {
     try {
-        const albums = await Album.find({ artist: req.params.artistId });
+        const artist = await Artist.findById(req.params.artistId);
+
+        if (!artist) {
+            return res.status(404).json({ message: 'Artist not found' });
+        }
+
+        const albums = artist.albums;
+
         res.json(albums);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
+        
