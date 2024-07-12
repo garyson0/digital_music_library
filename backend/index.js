5bd9fdcbe58a7644b84c6e08471d7601712546f9
garyson0 <vengergo@gmail.com>
2024-07-12 18:31:47 +0300

    fix/backend changed default port value of the api to 5000

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import artistRoutes from './routes/artistRoutes.js';
 
 const app = express();
 const dotEnv = dotenv.config();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 
 connectToDatabase();
