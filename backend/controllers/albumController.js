82d7d1823b1025dadabfe372410fbb2f744fda1f
garyson0 <vengergo@gmail.com>
2024-07-14 17:27:55 +0300

    fix/backend modified the albumcontroller to retrieve every information about the albums of an artist, not only the ids of them

--- backend/controllers/albumController.js
+++ backend/controllers/albumController.js
@@ -1,18 +1,22 @@
 import Artist from "../models/Artist.js";
+import Song from "../models/Song.js";
 
 export const getAlbumsByArtistId = async (req, res) => {
-    try {
-        const artist = await Artist.findById(req.params.artistId);
+  try {
+    const artist = await Artist.findById(req.params.artistId).populate({
+      path: "albums",
+      populate: {
+        path: "songs",
+      },
+    });
 
-        if (!artist) {
-            return res.status(404).json({ message: 'Artist not found' });
-        }
-
-        const albums = artist.albums;
-
-        res.json(albums);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    if (!artist) {
+      return res.status(404).json({ message: "Artist not found" });
     }
+
+    const albums = artist.albums;
+    res.json(albums);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
-        
